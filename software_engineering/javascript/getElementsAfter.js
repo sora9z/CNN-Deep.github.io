d101def776ae9d239559b8cbcd67e18263fc04b5
sora9z <sssssssorasss@gmail.com>
2021-07-18 09:36:21 +0900

    폴더 정리

--- software_engineering/javascript/getElementsAfter.js
+++
@@ -1,8 +0,0 @@
-function getElementsAfter(arr,n){
-
-    // slice를 사용하여 n+1 index 이후의 값을 갖는 새 배열을 리턴한다.
-    return arr.slice(n+1);
-}
-
-let output=getElementsAfter(['a','b','c','d','e'],2)
-console.log(output)
